b586b06f3bca84547f7b3582452c5b5395d431b4
Charly <charlyperezpop@gmail.com>
2024-07-05 19:11:10 -0600

    ADD: Steps on sign in

--- src/hooks/sign-up/use-sign-up.ts
+++ src/hooks/sign-up/use-sign-up.ts
@@ -1,10 +1,12 @@
+"use client"
+import { onCompleteUserRegistration } from "@/actions/auth"
 import { useToast } from "@/components/ui/use-toast"
 import { UserRegistrationProps, UserRegistrationSchema } from "@/schemas/auth.schema"
 import { useSignUp } from "@clerk/nextjs"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
 import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
 
 export const useSignUpForm = () =>{
     const {toast}= useToast()
@@ -38,5 +40,45 @@ export const useSignUpForm = () =>{
                 description: error.errors[0].longMessage,
             })
         }
+    }
+    const onHandleSubmit = methods.handleSubmit(
+        async (values: UserRegistrationProps)=>{
+            if(!isLoaded) return
+            try {
+                setLoading(true)
+                const completeSignUp = await signUp.attemptEmailAddressVerification({code: values.otp})
+                if(completeSignUp.status !== 'complete'){
+                    return {message:'Something went wrong!'}
+                }
+                if(completeSignUp.status == 'complete'){
+                    if(!signUp.createdUserId) return
+                    const registered = await onCompleteUserRegistration(values.fullname,signUp.createdUserId,values.type)
+                    if(registered?.status == 200 &&registered.user){
+                        await setActive({
+                            session: completeSignUp.createdSessionId,
+                        })
+                        setLoading(false)
+                        router.push('/dashboard')
+                    }
+                    if(registered?.status ==400){
+                        toast({
+                            title: 'Error',
+                            description: 'Something went wrong!',
+                        })
+                    }
+                }
+            }catch (error:any){
+                toast({
+                    title: 'Error',
+                    description: error.errors[0].longMessage,
+                })
+            }
+        } 
+    )
+    return{
+        methods,
+        onHandleSubmit,
+        onGenerateOTP,
+        loading,
     }
 }
