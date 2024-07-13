06af401ec6f2dce61f4812d2276ffde5424d717e
Charly <charlyperezpop@gmail.com>
2024-07-13 01:43:19 -0600

    ADD: Blogs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode:false,
+    images:{
+        remotePatterns:[
+            {
+                protocol: 'http',
+                hostname: '54.214.173.31'
+            }
+        ]
+    }
+};
 
 export default nextConfig;
