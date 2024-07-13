5163a85dd21e511ea2f12ad8fe1e0c85d1feb60a
Charly <charlyperezpop@gmail.com>
2024-07-13 01:57:37 -0600

    ADD: Setup billing settings page

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -1,10 +1,16 @@
+import InfoBar from '@/components/infobar'
 import React from 'react'
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
-    <div>page</div>
+    <>
+        <InfoBar />
+        <div className='overflow-y-auto w-full chat-window flex-1 h-0 flex flex-col gap-10'>
+            {/* <BillingSetting /> */}
+        </div>
+    </>
   )
 }
 
