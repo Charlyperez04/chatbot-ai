88af93f92f6c3b62619b47f17263d4fc1c9f19aa
Charly <charlyperezpop@gmail.com>
2024-07-21 23:34:25 -0600

    ADD: Domain management hooks

--- src/components/sidebar/domain-menu.tsx
+++ src/components/sidebar/domain-menu.tsx
@@ -1,4 +1,10 @@
+import { useDomain } from '@/hooks/sidebar/use-domain'
+import { cn } from '@/lib/utils'
 import React from 'react'
+import AppDrawer from '../drawer'
+import { Plus } from 'lucide-react'
+import Loader from '../loader'
+import FormGenerator from '../forms/form-generator'
 
 type Props = {
     min?: boolean
@@ -13,8 +19,39 @@ type Props = {
 }
 
 const DomainMenu = ({domains,min}: Props) => {
+  const {register, onAddDomain, loading, errors, isDomain} = useDomain()
   return (
-    <div>DomainMenu</div>
+    <div className={cn('flex flex-col gap-3', min ? 'mt-6': 'mt-3')}>
+      <div className='flex justify-between w-full items-center'>
+        {!min && <p className='text-xs text-gray-500'>DOMAINS</p>}
+        <AppDrawer
+         description='add in your domain address to integrate your chatbot'
+         title='Add yout bussiness domain'
+         onOpen={
+          <div className='cursor-pointer text-gray-500 rounded-full'>
+            <Plus/>
+          </div>
+         }
+        >
+          <Loader loading={loading}>
+            <form 
+             className='mt-3 w-6/12 flex flex-col gap-3'
+             onSubmit={onAddDomain}
+            >
+              <FormGenerator
+               inputType='input'
+               register={register}
+               label='Domain'
+                name='domain'
+               errors={errors}
+               placeholder='mydomain.com'
+               type='text'
+              />
+            </form>
+          </Loader>
+        </AppDrawer>
+      </div>
+    </div>
   )
 }
 
--- src/hooks/sidebar/use-domain.ts
+++ src/hooks/sidebar/use-domain.ts
@@ -1,9 +1,10 @@
+import { onIntegrateDomain } from '@/actions/settings'
 import { useToast } from '@/components/ui/use-toast'
 import { AddDomainSchema } from '@/schemas/settings.schema'
 import { zodResolver } from '@hookform/resolvers/zod'
 import {UploadClient} from '@uploadcare/upload-client'
 import { usePathname } from 'next/navigation'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import { FieldValues, useForm } from 'react-hook-form'
 
@@ -24,6 +25,29 @@ export const useDomain = () =>{
     const router = useRouter()
 
     useEffect(()=>{
-
+        setIsDomain(pathname.split('/').pop())
     },[pathname])
+
+    const onAddDomain = handleSubmit( async (values: FieldValues) =>{
+        setLoading(true)
+        const uploaded = await upload.uploadFile(values.image[0])
+        const domain = await onIntegrateDomain(values.domain,uploaded.uuid)
+        if(domain){
+            reset()
+            setLoading(false)
+            toast({
+                title: domain.status == 200 ? 'Success' : 'Error',
+                description: domain.message,
+            })
+            router.refresh()
+        }
+    })
+
+    return {
+        register,
+        onAddDomain,
+        errors,
+        loading,
+        isDomain
+    }
 }
