3a18fd2e75f7a36af619d8c20d1539fd2a6ce4b2
Charly <charlyperezpop@gmail.com>
2024-10-01 19:11:43 -0600

    Adding upload-button component

--- src/components/sidebar/domain-menu.tsx
+++ src/components/sidebar/domain-menu.tsx
@@ -1,51 +1,56 @@
 import { useDomain } from '@/hooks/sidebar/use-domain'
 import { cn } from '@/lib/utils'
-import React from 'react'
-import AppDrawer from '../drawer'
 import { Plus } from 'lucide-react'
-import Loader from '../loader'
+import AppDrawer from '../drawer'
 import FormGenerator from '../forms/form-generator'
+import Loader from '../loader'
+import UploadButton from '../upload-button'
 
 type Props = {
-    min?: boolean
-    domains:
-    | {
-        id:string
-        name:string
-        icon:string | null
-    }[]
-    | null
-    | undefined
+  min?: boolean
+  domains:
+  | {
+    id: string
+    name: string
+    icon: string | null
+  }[]
+  | null
+  | undefined
 }
 
-const DomainMenu = ({domains,min}: Props) => {
-  const {register, onAddDomain, loading, errors, isDomain} = useDomain()
+const DomainMenu = ({ domains, min }: Props) => {
+  const { register, onAddDomain, loading, errors, isDomain } = useDomain()
   return (
-    <div className={cn('flex flex-col gap-3', min ? 'mt-6': 'mt-3')}>
+    <div className={cn('flex flex-col gap-3', min ? 'mt-6' : 'mt-3')}>
       <div className='flex justify-between w-full items-center'>
         {!min && <p className='text-xs text-gray-500'>DOMAINS</p>}
         <AppDrawer
-         description='add in your domain address to integrate your chatbot'
-         title='Add yout bussiness domain'
-         onOpen={
-          <div className='cursor-pointer text-gray-500 rounded-full'>
-            <Plus/>
-          </div>
-         }
+          description='add in your domain address to integrate your chatbot'
+          title='Add yout bussiness domain'
+          onOpen={
+            <div className='cursor-pointer text-gray-500 rounded-full border-2'>
+              <Plus />
+            </div>
+          }
         >
           <Loader loading={loading}>
-            <form 
-             className='mt-3 w-6/12 flex flex-col gap-3'
-             onSubmit={onAddDomain}
+            <form
+              className='mt-3 w-6/12 flex flex-col gap-3'
+              onSubmit={onAddDomain}
             >
               <FormGenerator
-               inputType='input'
-               register={register}
-               label='Domain'
+                inputType='input'
+                register={register}
+                label='Domain'
                 name='domain'
-               errors={errors}
-               placeholder='mydomain.com'
-               type='text'
+                errors={errors}
+                placeholder='mydomain.com'
+                type='text'
+              />
+              <UploadButton
+                register={register}
+                label='Upload Icon'
+                errors={errors}
               />
             </form>
           </Loader>
